f80cfa3c6123701d38fe45968e9b5e9193d45649
Märten Soo <marten.soo@gmail.com>
2021-01-04 22:09:22 +0200

    Add files via upload

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,14 @@
-docker build -t stephengrider/multi-client:latest -t stephengrider/multi-client:$SHA -f ./client/Dockerfile ./client
-docker build -t stephengrider/multi-server:latest -t stephengrider/multi-server:$SHA -f ./server/Dockerfile ./server
-docker build -t stephengrider/multi-worker:latest -t stephengrider/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker build -t martensoo/multi-client:latest -t martensoo/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t martensoo/multi-server:latest -t martensoo/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t martensoo/multi-worker:latest -t martensoo/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 
-docker push stephengrider/multi-client:latest
-docker push stephengrider/multi-server:latest
-docker push stephengrider/multi-worker:latest
+docker push martensoo/multi-client:latest
+docker push martensoo/multi-server:latest
+docker push martensoo/multi-worker:latest
 
-docker push stephengrider/multi-client:$SHA
-docker push stephengrider/multi-server:$SHA
-docker push stephengrider/multi-worker:$SHA
+docker push martensoo/multi-client:$SHA
+docker push martensoo/multi-server:$SHA
+docker push martensoo/multi-worker:$SHA
 
 kubectl apply -f k8s
 kubectl set image deployments/server-deployment server=stephengrider/multi-server:$SHA
